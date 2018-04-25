3c5eda158735e4da03be2d79b7a805efecdc0438
Hermino Junior <herminojunior@gmail.com>
2018-04-24 20:38:07 -0400

    Comentários

--- Extração_das_Imgens.py
+++ Extração_das_Imgens.py
@@ -1,24 +1,32 @@
+'''
+Este algoritmo é reposável por separar em pastas as imagens de acordo com o rotulo definido
+na base de dados. Ex: Se uma imagem possui o rotulo 7, ela será classificada com surpresa e
+será colocada na pasta supresa.
+'''
+
 import glob
 from shutil import copyfile
 
-emotions = ["neutral", "anger", "contempt", "disgust", "fear", "happy", "sadness", "surprise"]
-participants = glob.glob("base\\source_emotion\\*")
+emotions = ["neutral", "anger", "contempt", "disgust", "fear", "happy", "sadness", "surprise"] # emoções
+participants = glob.glob("base\\source_emotion\\*") # retorna uma lista com os caminhos das pastas
 
-for x in participants:
+for x in participants: #itera sobre as pastas de source_emotion
     part = "%s" % x[-4:]
-    for sessions in glob.glob("%s\\*" % x):
-        for files in glob.glob("%s\\*" % sessions):
+    for sessions in glob.glob("{}\\*" % x):# itera sobre as subpastas de source emotion
+        for files in glob.glob("{}\\*" % sessions): # itera sobre cada imagem na subpasta
             current_session = files[25:-30]
 
             file = open(files, 'r')
 
             emotion = int(float(file.readline()))
 
-            sourcefile_emotion = glob.glob("base\\source_images\\{}\\{}\\*".format(part, current_session))[-1]
-            sourcefile_neutral = glob.glob("base\source_images\\%s\\%s\\*" % (part, current_session))[0]
+            sourcefile_emotion = glob.glob("base\\source_images\\{}\\{}\\*".format(part, current_session))[-1] # pega a ultima imagem, a que possui a emoção bem definida
+            sourcefile_neutral = glob.glob("base\source_images\\{}\\{}\\*" % (part, current_session))[0] # pega a primeira imagem, neutra
+            
+            # As variaveis acima guardam o caminho das imagem obtidas, logo abaixo o mesmo procedimento
+            
+            dest_neut = "base\sorted_set\\neutral\\{}" % sourcefile_neutral[28:] # local para armazenamento da imagem neutra
+            dest_emot = "base\sorted_set\\{}\\{}" % (emotions[emotion], sourcefile_emotion[28:]) #local para armazenamento da imagem da emoção bem definida
 
-            dest_neut = "base\sorted_set\\neutral\\%s" % sourcefile_neutral[28:]
-            dest_emot = "base\sorted_set\\%s\\%s" % (emotions[emotion], sourcefile_emotion[28:])
-
-            copyfile(sourcefile_neutral, dest_neut)
+            copyfile(sourcefile_neutral, dest_neut) # Realiza copia para o destino
             copyfile(sourcefile_emotion, dest_emot)
